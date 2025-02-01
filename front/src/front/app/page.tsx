520b9af33fc9eccdb78d38399cf6a04af151bcd6
mattuu0 <tempacountyt@gmail.com>
2025-02-01 14:28:08 +0900

    最初のコミット

---
+++ front/src/front/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <div>
+            <div className="container">
+                <div className="card shadow-sm p-2 m-2" >
+                    <h1 className="text-3xl">ポートフォリオ</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
